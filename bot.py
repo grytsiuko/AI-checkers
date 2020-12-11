1a6c5a79d06ce2e3ce6efd5d0e5d5c1e6aab43d4
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-12-11 21:21:11 +0200

    Waiting for the first move

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import aiohttp
 
 from env import SERVER_URL
+from game import Game
 
 
 class Bot:
@@ -10,6 +11,8 @@ class Bot:
         self._color = None
         self._token = None
         self._session = None
+        self._game = Game()
+        self._state = None
 
     def start(self):
         loop = asyncio.new_event_loop()
@@ -21,8 +24,16 @@ class Bot:
         await self._connect()
 
         print(f'Bot {self._name} init - {self._color}, {self._token}')
+        await self._play()
 
         await self._session.close()
+
+    async def _play(self):
+        while True:
+            await self._refresh_state()
+            if self._state['whose_turn'] == self._color:
+                break
+        print(f'Bot {self._name} is moving')
 
     async def _connect(self):
         async with self._session.post(
@@ -32,3 +43,10 @@ class Bot:
             res = (await resp.json())['data']
             self._color = res['color']
             self._token = res['token']
+
+    async def _refresh_state(self):
+        async with self._session.get(
+                f'{SERVER_URL}/game'
+        ) as resp:
+            res = (await resp.json())['data']
+            self._state = res
