bd2bf058d7c0232b021f858d3bcd8c09d1a2c306
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-12-20 21:50:46 +0200

    Copied mini max to iterative

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from heuristics.amount_heuristic import AmountHeuristic
 from heuristics.generic_heuristic import GenericHeuristic
 from heuristics.legendary_heuristic import LegendaryHeuristic
+from mini_max.mini_max_iterative import MiniMaxIterative
 
 load_dotenv()
 
@@ -12,7 +13,7 @@ from bot import Bot
 
 if __name__ == '__main__':
     a = threading.Thread(target=Bot("A", 4).start)
-    b = threading.Thread(target=Bot("B", 4).start)
+    b = threading.Thread(target=Bot("B", 2, mini_max_class=MiniMaxIterative).start)
 
     a.start()
     b.start()
---
+++ mini_max/mini_max_iterative.py
@@ -0,0 +1,76 @@
+import random
+
+from heuristics.current_position_statistics import CurrentPositionStatistics
+from heuristics.position_statistics import PositionStatistics
+
+
+class MiniMaxIterative:
+
+    def __init__(self, board, meta_info, depth, heuristic):
+        self._board = board
+        self._meta_info = meta_info
+        self._depth = depth
+        self._heuristic = heuristic
+        self._queue = None
+
+    def find_best_move(self):
+        best_move = None
+        best_score = None
+        possible_moves = self._board.get_possible_moves()
+        if len(possible_moves) == 1:
+            return possible_moves[0]
+
+        random.shuffle(possible_moves)
+
+        for move in possible_moves:
+            score = self._alpha_beta_call(
+                move,
+                self._depth,
+                float('-inf') if best_score is None else best_score,
+                float("inf")
+            )
+            if best_score is None or best_score < score:
+                best_score = score
+                best_move = move
+        return best_move
+
+    def _alpha_beta_call(self, move, depth, alpha, beta):
+        was_captured = self._board.do_move(move)
+        new_depth = depth if self._board.piece_requiring_further_capture_moves is not None else depth - 1  # todo if capture_moves more than 1 it can take some time
+        score = self._alpha_beta(new_depth, alpha, beta)
+        self._board.do_reverse_move(move, was_captured)
+        return score
+
+    def _alpha_beta(self, depth: int, alpha: float, beta: float):
+        if depth <= 0 and len(self._board.get_possible_capture_moves()) == 0:
+            return self._apply_heuristic()
+
+        if self._meta_info.self_number == self._board.player_turn:
+            value = float("-inf")
+            possible_moves = self._board.get_possible_moves()
+            random.shuffle(possible_moves)
+
+            for move in possible_moves:
+                value = max(value, self._alpha_beta_call(move, depth, alpha, beta))
+                alpha = max(alpha, value)
+            return value
+
+        else:
+            value = float("inf")
+            possible_moves = self._board.get_possible_moves()
+            random.shuffle(possible_moves)
+
+            for move in possible_moves:
+                value = min(value, self._alpha_beta_call(move, depth, alpha, beta))
+                beta = min(beta, value)
+            return value
+
+    def _apply_heuristic(self):  # todo make static?
+        self_statistics = CurrentPositionStatistics(self._board, self._meta_info.self_number)
+        opponent_statistics = PositionStatistics(self._board, self._meta_info.opponent_number)
+
+        heuristic = self._heuristic.calculate(
+            self_statistics=self_statistics, opponent_statistics=opponent_statistics
+        )
+
+        return heuristic
