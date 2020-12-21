e52db26bf5f410f3eb96ea6a4b95398ae3551190
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-12-21 02:53:38 +0200

    Init SpaceHeuristic

--- heuristics/legendary_heuristic.py
+++ heuristics/legendary_heuristic.py
@@ -4,4 +4,4 @@ from heuristics.generic_heuristic import GenericHeuristic
 class LegendaryHeuristic(GenericHeuristic):
 
     def __init__(self):
-        super().__init__([(12.424879018719095, 1), (16.263504183839, 1), (-8.663468683373988, 1), (-5.705007490296328, 1)])
+        super().__init__([(12.424879018719095, 1), (16.263504183839, 1), (-8.663468683373988, 1), (-5.705007490296328, 1), (0, 0), (0, 0), (0, 0), (0, 0)])
---
+++ heuristics/space_heuristic.py
@@ -0,0 +1,7 @@
+from heuristics.generic_heuristic import GenericHeuristic
+
+
+class SpaceHeuristic(GenericHeuristic):
+
+    def __init__(self):
+        super().__init__([(12.036080325807538, 1), (14.440556624041355, 1), (-8.02422920639554, 1), (-5.630438776072073, 1), (0.19150817138070297, 1), (-0.18818505411897024, 1), (0.28210919274461155, 1), (-0.03243033492781887, 1)])
